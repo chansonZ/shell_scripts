23460274dcd415f398e95f012da1443b712b7b2f
Jo-fai Chow <jofai.chow@gmail.com>
2015-05-11 22:38:01 +0100

    first commit

---
+++ setup_ubuntu_nvidia.sh
@@ -0,0 +1,4 @@
+## http://angrystatistician.blogspot.ca/2015/02/short-notes-get-cuda-and-gputools.html?utm_content=buffera5096&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer
+
+sudo add-apt-repository -y ppa:xorg-edgers/ppa
+sudo apt-get update && sudo apt-get install -y nvidia-346
---
+++ upgrade_ubuntu_kernel_4.sh
@@ -0,0 +1,9 @@
+cd /tmp/
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000_4.0.0-040000.201504121935_all.deb
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-headers-4.0.0-040000-generic_4.0.0-040000.201504121935_amd64.deb
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0-vivid/linux-image-4.0.0-040000-generic_4.0.0-040000.201504121935_amd64.deb
+
+sudo dpkg -i linux-headers-4.0.0-*.deb linux-image-4.0.0-*.deb
