a246affda88eb620e7aa4c06dcd5a7f5b00b77c6
Jo-fai Chow <jofai.chow@gmail.com>
2015-05-21 09:56:55 +0100

    Update setup_R_cloud.sh
    Added more stuff

--- setup_R_cloud.sh
+++ setup_R_cloud.sh
@@ -5,8 +5,44 @@
 #gpasswd -a woobe sudo
 #su - woobe
 
-## Update
+
+## ==============================================================================
+## Add repo
+## ==============================================================================
+
+# R
+sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+
+# Java
+sudo add-apt-repository -y ppa:webupd8team/java
+
+
+## ==============================================================================
+## Update and Core Packages
+## ==============================================================================
+
 sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install preload build-essential
+
+
+## ==============================================================================
+## Swap
+## ==============================================================================
+
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
+sudo sh -c 'echo "/var/swap.1 swap swap defaults 0 0 " >> /etc/fstab'
+sudo bash -c "echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
+
+## Java (OK)
+echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get -y install oracle-java9-installer oracle-java9-set-default
+
+## Git (OK)
+sudo apt-get -y install git && git config --global user.name "Jo-fai Chow" && git config --global user.email jofai.chow@gmail.com
 
 ## Web Server
 sudo apt-get -y install nginx
@@ -16,17 +52,17 @@ sudo apt-get -y install nginx
 #sudo service nginx start
 #sudo service nginx restart
 
-## Install R
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
-gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
-sudo apt-get update && sudo apt-get -y install r-base
+## ==============================================================================
+## R
+## ==============================================================================
 
-## Add Swap
-sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-sudo /sbin/mkswap /var/swap.1
-sudo /sbin/swapon /var/swap.1
-sudo sh -c 'echo "/var/swap.1 swap swap defaults 0 0 " >> /etc/fstab'
+## Install R
+sudo apt-get -y install r-base r-base-core r-base-dev r-recommended
+sudo apt-get -y install ssh-askpass libopenblas-base libcurl4-openssl-dev libxml2-dev libtiff4-dev libxt-dev
+sudo apt-get -y install libgdal1-dev libproj-dev
+sudo apt-get -y install libglu1-mesa-dev
+## for EBImage
+sudo apt-get -y install libfftw3-dev
 
 ## Libraries for devtools
 sudo apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
@@ -38,13 +74,22 @@ sudo su - -c "R -e \"install.packages('rmarkdown', repos='http://cran.rstudio.co
 
 ## RStudio Server
 sudo apt-get -y install libapparmor1 gdebi-core
-wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-0.99.435-amd64.deb
-sudo gdebi rstudio-server-0.99.435-amd64.deb
+wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-0.99.441-amd64.deb
+sudo gdebi rstudio-server-0.99.441-amd64.deb
 
 ## Shiny Server
 wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb
 sudo gdebi shiny-server-1.3.0.403-amd64.deb
 
-## Give permission to folders
-sudo chmod -R 777 /srv
-sudo chmod -R 777 /usr
+## ==============================================================================
+## Other Stuff
+## ==============================================================================
+
+## Pandoc (OK)
+sudo apt-get -y install pandoc
+
+## Compression
+sudo apt-get install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
+
+## TeX stuff for pdf (OK)
+sudo apt-get -y install texlive-latex-base texlive-fonts-recommended texlive-full
