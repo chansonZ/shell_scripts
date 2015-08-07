044f999cef67b1e0eddb4deb5ef43a41e663765c
Jo-fai Chow <jofai.chow@gmail.com>
2015-08-08 00:04:21 +0100

    Update setup_R_cloud_extra_pkg.sh
    Added more packages for shiny apps

--- setup_R_cloud_extra_pkg.sh
+++ setup_R_cloud_extra_pkg.sh
@@ -4,3 +4,12 @@
 
 sudo su - -c "R -e \"devtools::install_github('ramnathv/htmlwidgets')\""
 sudo su - -c "R -e \"devtools::install_github('bokeh/rbokeh')\""
+sudo su - -c "R -e \"install.packages('base64enc', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"devtools::install_github('ramnathv/rCharts@dev')\""
+sudo su - -c "R -e \"devtools::install_github('ramnathv/rMaps')\""
+sudo su - -c "R -e \"install.packages('ggmap', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('shinydashboard', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('data.table', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('dplyr', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('rvest', repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"devtools::install_github('htmlwidgets/sparkline')\""
